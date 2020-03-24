989540d0f4268f6da3b8f59f43752024539d6673
batyshkaLenin <auroragamesproject@gmail.com>
2020-03-24 22:57:13 +0700

    Updates

---
+++ precache-manifest.8a91dc6f4b2e816808cf4e2891517825.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f325310625b976ccd9d3e8665ca2fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "088595bcf9a42e3cd160",
+    "url": "/static/css/main.5cf81fa5.chunk.css"
+  },
+  {
+    "revision": "0b7a3ec9d276f20e0de6",
+    "url": "/static/js/2.d478de13.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d478de13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "088595bcf9a42e3cd160",
+    "url": "/static/js/main.b5e17ae8.chunk.js"
+  },
+  {
+    "revision": "b6f5aaa1ed4c45abf8f4",
+    "url": "/static/js/runtime-main.093e0c76.js"
+  }
+]);

c65b58431fe08cc03d6b026634865d40e1afa839
batyshkaLenin <auroragamesproject@gmail.com>
2020-04-05 17:15:30 +0700

    The party ordered to release the new version without fail

---
+++ precache-manifest.588d36e72f572ba0a139439b78ecd2e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b9b12a44ab45428f5390af7c8c5beb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d74961e5f11fc1d0e7a9",
+    "url": "/static/css/main.c361ca45.chunk.css"
+  },
+  {
+    "revision": "0b7a3ec9d276f20e0de6",
+    "url": "/static/js/2.d478de13.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d478de13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d74961e5f11fc1d0e7a9",
+    "url": "/static/js/main.f7fdfa4b.chunk.js"
+  },
+  {
+    "revision": "b6f5aaa1ed4c45abf8f4",
+    "url": "/static/js/runtime-main.093e0c76.js"
+  }
+]);
